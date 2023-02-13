e5db1430b03713e10d027e44c5c44f47a17dc314
luan-carvalho <ita.luan12@gmail.com>
2023-02-13 09:36:39 -0300

    SellerDaoJDBC and DaoFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,8 +1,13 @@
 package application;
 
+import model.dao.FactoryDao;
+import model.dao.SellerDao;
+
 public class Program {
 
 	public static void main(String[] args) {
+		
+		SellerDao sellerDao = FactoryDao.createSellerDao();
 
 	}
 
